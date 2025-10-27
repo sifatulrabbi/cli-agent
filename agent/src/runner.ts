b857aab9aec898606eaa368b6a31c20e9ef9ddf0
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-10-27 12:52:29 +0600

    feat: use stdin and stdout to communicate

--- agent/src/runner.ts
+++ agent/src/runner.ts
@@ -1,8 +1,99 @@
 #!/usr/bin/env bun
 
+import { z } from "zod";
+import { v4 } from "uuid";
+import { concat } from "@langchain/core/utils/stream";
+import { workflow } from "../src/workflow";
+import { type BaseMessage } from "@langchain/core/messages";
+
+type MsgEvent = {
+  event: "delta" | "end" | "start";
+  history: {
+    id: string;
+    reasoning: string;
+    content: string;
+    toolName: string;
+    toolArgs: string;
+  }[];
+};
+
+const inputSchema = z.object({
+  workingPath: z.string(),
+  userInput: z.string(),
+});
+
+function sendEvent(ev: MsgEvent) {
+  Bun.stdout.write(JSON.stringify(ev) + "\n");
+}
+
 for await (const line of console) {
-  console.error("  Got:", line);
-  process.stdout.write(
-    `{"type": "ai", "content": "Hello world", "input": "${line}"}\n`,
+  const result = inputSchema.parse(JSON.parse(line));
+
+  const stream = await workflow.stream(
+    { userInput: result.userInput },
+    { streamMode: "messages" },
   );
+  sendEvent({
+    event: "start",
+    history: [
+      {
+        id: v4(),
+        reasoning: "",
+        content: result.userInput,
+        toolName: "",
+        toolArgs: "",
+      },
+    ],
+  });
+
+  const chunks: BaseMessage[] = [];
+  let idx = -1;
+
+  for await (const [message, _metadata] of stream) {
+    if (!message.id) {
+      continue;
+    }
+
+    if (chunks[idx] && chunks[idx]!.id === message.id) {
+      chunks[idx] = concat(chunks[idx]!, message);
+    } else {
+      chunks.push(message);
+      idx++;
+    }
+
+    if (message.type !== "ai") {
+      continue;
+    }
+
+    message.contentBlocks.forEach((block) => {
+      if (block.type.includes("reasoning") && block.reasoning) {
+        sendEvent({
+          event: "delta",
+          history: [
+            {
+              id: message.id!,
+              reasoning: block.reasoning as string,
+              content: "",
+              toolName: "",
+              toolArgs: "",
+            },
+          ],
+        });
+      }
+      if (block.type.includes("text") && block.text) {
+        sendEvent({
+          event: "delta",
+          history: [
+            {
+              id: message.id!,
+              reasoning: "",
+              content: block.text as string,
+              toolName: "",
+              toolArgs: "",
+            },
+          ],
+        });
+      }
+    });
+  }
 }
--- cmd/test_connection/main.go
+++ cmd/test_connection/main.go
@@ -12,35 +12,54 @@ import (
 	"strings"
 )
 
-type Msg struct {
-	Type  string `json:"type"`
-	ID    string `json:"id,omitempty"`
-	Input string `json:"input,omitempty"`
-	Delta string `json:"delta,omitempty"`
-	Final string `json:"final,omitempty"`
-	Error string `json:"error,omitempty"`
+type EventType = string
+
+type HistoryMessage struct {
+	ID        string `json:"id"`
+	Reasoning string `json:"reasoning"`
+	Content   string `json:"content"`
+	ToolName  string `json:"toolName"`
+	ToolArgs  string `json:"toolArgs"`
 }
 
+type MsgEvent struct {
+	Event       EventType        `json:"event"` // end | delta | start
+	WorkingPath string           `json:"workingPath,omitempty"`
+	UserInput   string           `json:"userInput,omitempty"`
+	History     []HistoryMessage `json:"history,omitempty"`
+}
+
+const (
+	EventStart EventType = "start"
+	EventDelta EventType = "delta"
+	EventEnd   EventType = "end"
+)
+
 func main() {
+	workingPath, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Unable to get the current working directory:", err)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	agentPath := "./agent/src/runner.ts"
-	_, err := os.Open(agentPath)
+	_, err = os.Open(agentPath)
 	if err != nil {
 		log.Fatalln("File does not exist", err)
 	}
 
 	cmd := exec.CommandContext(ctx, "bun", agentPath)
-	stdin, err := cmd.StdinPipe()
+	serverStdin, err := cmd.StdinPipe()
 	if err != nil {
 		log.Fatalln("Unable to pipe the stdin:", err)
 	}
-	stdout, err := cmd.StdoutPipe()
+	serverStdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatalln("Unable to pipe the stdout:", err)
 	}
-	stderr, err := cmd.StderrPipe()
+	serverStderr, err := cmd.StderrPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,29 +71,25 @@ func main() {
 	}
 
 	// Forward agent's stderr to ours for debugging
-	go io.Copy(os.Stdout, stderr)
+	go io.Copy(os.Stderr, serverStderr)
+
+	go readAndPrintServerEvents(serverStdout)
 
 	go func() {
-		rd := bufio.NewReader(stdout)
-		for {
-			line, err := rd.ReadBytes('\n')
-			if err != nil {
-				log.Fatalln("Error reading the lines:", err)
-			}
-			fmt.Println("AI:", string(line))
-		}
-	}()
-
-	go func() {
-		enc := json.NewEncoder(stdin)
+		enc := json.NewEncoder(serverStdin)
 		enc.SetEscapeHTML(false)
-		reader := bufio.NewReader(os.Stdin)
 
+		reader := bufio.NewReader(os.Stdin)
 		for {
-			fmt.Print("USER: ")
 			input, _ := reader.ReadString('\n')
 			input = strings.TrimSpace(input) // remove trailing newline
-			if err = enc.Encode(Msg{Type: "user", ID: "1", Input: input}); err != nil {
+
+			if err = enc.Encode(MsgEvent{
+				WorkingPath: workingPath,
+				Event:       EventStart,
+				UserInput:   input,
+				History:     []HistoryMessage{},
+			}); err != nil {
 				log.Fatalln("Error when sending data thru stdin:", err)
 			}
 		}
@@ -85,4 +100,75 @@ func main() {
 		log.Fatalln(err)
 	}
 	<-ctx.Done()
+}
+
+func readAndPrintServerEvents(serverStdout io.Reader) {
+	var msgCache []HistoryMessage
+
+	rd := bufio.NewReader(serverStdout)
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err != nil {
+			log.Fatalln("Error reading the lines:", err)
+		}
+
+		var msgEvent MsgEvent
+		if err := json.Unmarshal(line, &msgEvent); err != nil {
+			log.Fatalln("Invalid event from the server:", err, "\nEvent:", string(line))
+		}
+
+		switch msgEvent.Event {
+		case EventStart:
+			msgCache = append(msgCache, msgEvent.History...)
+
+		case EventDelta:
+			for i := len(msgEvent.History) - 1; i >= 0; i-- {
+				delta := msgEvent.History[i]
+				last := -1
+
+				for i, msg := range msgCache {
+					if msg.ID == delta.ID {
+						last = i
+						break
+					}
+				}
+
+				if last < 0 {
+					msgCache = append(msgCache, delta)
+					continue
+				}
+
+				cachedMsg := msgCache[last]
+
+				if delta.Reasoning != "" {
+					cachedMsg.Reasoning = delta.Reasoning
+				}
+				if delta.Content != "" {
+					cachedMsg.Content = delta.Content
+				}
+				if delta.ToolName != "" {
+					cachedMsg.ToolName = delta.ToolName
+				}
+				if delta.ToolArgs != "" {
+					cachedMsg.ToolArgs = delta.ToolArgs
+				}
+
+				msgCache[last] = cachedMsg
+				last--
+			}
+
+		case EventEnd:
+			for i := len(msgEvent.History) - 1; i >= 0; i-- {
+				delta := msgEvent.History[i]
+				for i, msg := range msgCache {
+					if msg.ID == delta.ID {
+						msgCache[i] = delta
+						break
+					}
+				}
+			}
+		}
+
+		fmt.Printf("History count: %d.\nDelta: %s\n", len(msgCache), msgEvent)
+	}
 }
